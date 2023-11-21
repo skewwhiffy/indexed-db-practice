447204b02404c96aaa8f41735b4957c2e953ecfe
Kenny Hung (he) <kennysubs+github@outlook.com>
2023-11-21 17:47:16 +0000

    Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {FormEvent, useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default () => {
+    const [item, setItem] = useState('')
+    
+    const submit = (e: FormEvent) => {
+        e.preventDefault()
+        console.log(e)
+    }
 
-export default App;
+    return (
+        <div className="App">
+            <form onSubmit={e => submit(e)}>
+                <label htmlFor="todo-item">TODO</label>
+                <input id="todo-item" value={item} onChange={e => setItem(e.target.value)} type="text"/>
+                <button>Submit</button>
+            </form>
+        </div>
+    );
+}
